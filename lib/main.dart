3f2efad1646c8610dacb6491f8e70363fb9506fd
YilmazBatal <b.ogulbey@gmail.com>
2024-06-06 23:03:37 +0300

    OnBoarding And Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
-import 'package:final_project/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:final_project/app.dart';
-import 'package:final_project/cubit/app_cubit.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'cubit/app_cubit.dart';
 import 'cubit/app_state.dart';
 import 'localization/app_localization.dart';
 import 'router.dart';
+import 'themes/app_themes.dart';
 
 void main() async {
-  // Flutter framework'ünü başlat
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Bildirim iznini kontrol et ve gerekirse iste
   await Permission.notification.isDenied.then((value) {
     if (value) {
       Permission.notification.request();
     }
   });
 
-  // Cihazın yönünü yalnızca dikey olarak ayarlayın
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  // Uygulamayı başlat
   runApp(const MainApp());
 }
 
--- lib/screens/boarding_screen.dart
+++ lib/screens/boarding_screen.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
 
 import '../localization/app_localization.dart';
+// Text(AppLocalizations.of(context).getTranslate("boarding_screen")));
+class BoardingScreen extends StatefulWidget {
+  const BoardingScreen({super.key});
 
-class BoardingScreen extends StatelessWidget {
+  @override
+  State<BoardingScreen> createState() => _BoardingScreenState();
+}
+
+class _BoardingScreenState extends State<BoardingScreen> {
+  List<Slide> slides = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      Slide(
+        title: "Welcome",
+        description: "This is an onboarding screen",
+        pathImage: "assets/onboarding1.png",
+      ),
+    );
+    slides.add(
+      Slide(
+        title: "Discover",
+        description: "Discover new features",
+        pathImage: "assets/onboarding2.png",
+      ),
+    );
+    slides.add(
+      Slide(
+        title: "Get Started",
+        description: "Let's get started!",
+        pathImage: "assets/onboarding3.png",
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // Navigate to your main screen
+    Navigator.pushReplacementNamed(context, "/main");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child:
-            Text(AppLocalizations.of(context).getTranslate("boarding_screen")));
+    return IntroSlider(
+      slides: slides,
+      onDonePress: onDonePress,
+    );
   }
 }
